bd6605243abf7af1cfd9652fab8bcfc5aa38d9d9
carlosrs14 <crincones578@gmail.com>
2026-06-07 20:45:06 -0500

    celcuis -> fahrenheit

---
+++ 01_data_types/03_celsius_fahrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float c;
+
+    printf("celcius: ");
+    
+    if (scanf("%f", &c) == 1) {
+        printf("fahrenheit: %.2f\n", c * 1.8f + 32.0f);
+    }
+    return 0;
+}
