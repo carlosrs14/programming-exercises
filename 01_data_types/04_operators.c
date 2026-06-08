2f6b733007cf4c285914c012861cf02f77ee3964
carlosrs14 <crincones578@gmail.com>
2026-06-07 20:49:32 -0500

    operators

---
+++ 01_data_types/04_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("A: ");
+    scanf("%d", &a);
+    printf("B: ");
+    scanf("%d", &b);
+        
+    printf("(A + B): %d\n", a + b);
+    printf("(A - B): %d\n", a - b);
+    printf("(A * B): %d\n", a * b);
+
+    if (b != 0) {
+        printf("(A / B): %d\n", a / b);
+        printf("(A %% B): %d\n", a % b);
+    } else {
+        perror("Zero divisor");
+    }
+    return 0;
+}
