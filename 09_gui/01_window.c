2bdd2fe89c0030d8af8ba933be950278d14ebe25
carlosrs14 <crincones578@gmail.com>
2026-06-11 00:55:21 -0500

    refactor into functions

--- 09_gui/01_window.c
+++ 09_gui/01_window.c
@@ -1,33 +1,67 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-#define WIDTH 600
+#define TITLE "open window"
+#define WIDTH HEIGTH
 #define HEIGTH 600
 
+struct Game {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+int sdl_initialize(struct Game* game);
+void sdl_quit(struct Game* game);
+
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
+    struct Game game = {
+        .window = NULL,
+        .renderer = NULL
+    };
 
-    SDL_Window* window = SDL_CreateWindow(
-        "window",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        WIDTH, HEIGTH, 0
-    );
-
-    SDL_Event event;
-
-    short running = 1;
-
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = 0;
-        }
-        SDL_Delay(16);
+    if (sdl_initialize(&game) == -1) {
+        sdl_quit(&game);
+        perror("error sdl");
+        return 1;
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    SDL_RenderClear(game.renderer);
+
+    SDL_RenderPresent(game.renderer);
+
+    SDL_Delay(5000);
+
+    sdl_quit(&game);
 
     return 0;
 }
 
+int sdl_initialize(struct Game *game) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        return -1;
+    }
+    
+    game->window = SDL_CreateWindow(
+        TITLE,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH, HEIGTH, 0
+    );
+    if (!game->window) {
+        return -1;
+    }
+
+    game->renderer = SDL_CreateRenderer(game->window, -1, 0);
+    if (!game->renderer) {
+        return -1;
+    }
+
+    return 0;
+}
+
+void sdl_quit(struct Game *game) {
+    SDL_DestroyRenderer(game->renderer);
+    SDL_DestroyWindow(game->window);
+    SDL_Quit();
+    exit(0);
+}
--- 09_gui/02_button.c
+++ 09_gui/02_button.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+#define TITLE "show button"
 #define WIDTH 600
 #define HEIGTH 600
+
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+} Game;
 
 typedef struct {
     SDL_Rect rect;
     short pressed;
 } Button;
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow(
-        "window",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 
-        WIDTH, HEIGTH, 0
-    );
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+int sdl_initialize(Game* game);
+void sdl_quit(Game* game);
+
+int main() {
+    Game game = {
+        .window = NULL,
+        .renderer = NULL
+    };
 
     Button b = {
         .rect = {250, 250, 100, 50},
@@ -27,6 +32,10 @@ int main() {
         
     SDL_Event event;
     short running = 1;
+
+    if (sdl_initialize(&game) == -1) {
+        return 1;
+    }
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -47,23 +56,52 @@ int main() {
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(game.renderer, 0, 0, 0, 255);
+        SDL_RenderClear(game.renderer);
 
         if (b.pressed)
-            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_SetRenderDrawColor(game.renderer, 0, 255, 0, 255);
         else
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_SetRenderDrawColor(game.renderer, 255, 0, 0, 255);
 
-        SDL_RenderFillRect(renderer, &b.rect);
+        SDL_RenderFillRect(game.renderer, &b.rect);
 
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(game.renderer);
 
         SDL_Delay(16);
     }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    
+    sdl_quit(&game);
 
     return 0;
+}
+
+int sdl_initialize(Game *game) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        return -1;
+    }
+    
+    game->window = SDL_CreateWindow(
+        TITLE,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH, HEIGTH, 0
+    );
+    if (!game->window) {
+        return -1;
+    }
+
+    game->renderer = SDL_CreateRenderer(game->window, -1, 0);
+    if (!game->renderer) {
+        return -1;
+    }
+
+    return 0;
+}
+
+void sdl_quit(Game *game) {
+    SDL_DestroyRenderer(game->renderer);
+    SDL_DestroyWindow(game->window);
+    SDL_Quit();
+    exit(0);
 }
